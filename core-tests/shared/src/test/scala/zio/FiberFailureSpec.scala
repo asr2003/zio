9f33f28fb8eb257ff5fa21bb548d774373f2b11a
asr2003 <162500856+asr2003@users.noreply.github.com>
2024-08-12 00:58:15 +0530

    zio.fail Throwable

--- core-tests/shared/src/test/scala/zio/FiberFailureSpec.scala
+++ core-tests/shared/src/test/scala/zio/FiberFailureSpec.scala
@@ -34,6 +34,35 @@ object FiberFailureSpec extends ZIOBaseSpec {
           )
         }
       }
-    } @@ exceptJS
+    } @@ exceptJS,
+      test ("FiberFailure captures the stack trace for ZIO.fail with Throwable") {
+        def subcall(): Unit =
+          Unsafe.unsafe { implicit unsafe =>
+            Runtime.default.unsafe.run(ZIO.fail(new Exception("boom"))).getOrThrowFiberFailure()
+          }
+        def call1(): Unit = subcall()
+
+        val fiberFailureTest = ZIO
+          .attempt(call1())
+          .catchAll {
+            case fiberFailure: FiberFailure =>
+              val stackTrace = fiberFailure.getStackTrace.mkString("\n")
+              ZIO.log(s"Captured Stack Trace:\n$stackTrace") *>
+                ZIO.succeed(stackTrace)
+            case other =>
+              ZIO.succeed(s"Unexpected failure: ${other.getMessage}")
+          }
+          .asInstanceOf[ZIO[Any, Nothing, String]]
+
+        fiberFailureTest.flatMap { stackTrace =>
+          ZIO.succeed {
+            assertTrue(
+              stackTrace.contains("call1") &&
+                stackTrace.contains("subcall") &&
+                stackTrace.contains("FiberFailureSpec")
+            )
+          }
+        }
+      } @@ exceptJS
   )
 }
