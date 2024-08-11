f16afb49fd8d1b047e85b17f9828e895b7817001
asr2003 <162500856+asr2003@users.noreply.github.com>
2024-08-11 23:11:20 +0530

    Update FiberFailureSpec.scala

--- core-tests/shared/src/test/scala/zio/FiberFailureSpec.scala
+++ core-tests/shared/src/test/scala/zio/FiberFailureSpec.scala
@@ -7,15 +7,15 @@ object FiberFailureSpec extends ZIOBaseSpec {
 
   def spec =
     suite("FiberFailureSpec")(
-      //   test("captures full stack trace including user code for ZIO.fail with String") {
-      //     def subcall(): Unit =
-      //       Unsafe.unsafe { implicit unsafe =>
-      //         Runtime.default.unsafe.run(ZIO.fail("boom")).getOrThrowFiberFailure()
-      //       }
-      //     def call1(): Unit = subcall()
+      test("captures full stack trace including user code for ZIO.fail with String") {
+        def subcall(): Unit =
+          Unsafe.unsafe { implicit unsafe =>
+            Runtime.default.unsafe.run(ZIO.fail("boom")).getOrThrowFiberFailure()
+          }
+        def call1(): Unit = subcall()
 
-      //     assertStackTraceConsistency(() => call1())
-      //   },
+        assertStackTraceConsistency(() => call1())
+      },
       //   test("captures full stack trace including user code for ZIO.fail with Throwable") {
       //     def subcall(): Unit =
       //       Unsafe.unsafe { implicit unsafe =>
@@ -83,48 +83,52 @@ object FiberFailureSpec extends ZIOBaseSpec {
 
   private def assertStackTraceConsistency(call1: () => Unit) =
     for {
-      fiberFailureTest: ZIO[Any, Nothing, (String, String, String)] <- ZIO
-                                                                         .attempt(call1())
-                                                                         .catchAll { case fiberFailure: FiberFailure =>
-                                                                           val stackTrace = cleanStackTrace(
-                                                                             fiberFailure.getStackTrace.mkString("\n")
-                                                                           )
-                                                                           val toStringOutput =
-                                                                             cleanStackTrace(fiberFailure.toString)
-                                                                           val printStackTraceOutput = {
-                                                                             val stream = new ByteArrayOutputStream()
-                                                                             fiberFailure.printStackTrace(
-                                                                               new PrintStream(stream)
-                                                                             )
-                                                                             cleanStackTrace(stream.toString)
-                                                                           }
-                                                                           ZIO.succeed(
-                                                                             (
-                                                                               stackTrace,
-                                                                               toStringOutput,
-                                                                               printStackTraceOutput
-                                                                             )
-                                                                           )
-                                                                         }
+      fiberFailureTest <- ZIO
+                            .attempt(call1())
+                            .catchAll { case fiberFailure: FiberFailure =>
+                              val stackTrace = cleanStackTrace(
+                                fiberFailure.getStackTrace.mkString("\n")
+                              )
+                              val toStringOutput =
+                                cleanStackTrace(fiberFailure.toString)
+                              val printStackTraceOutput = {
+                                val stream = new ByteArrayOutputStream()
+                                fiberFailure.printStackTrace(
+                                  new PrintStream(stream)
+                                )
+                                cleanStackTrace(stream.toString)
+                              }
+                              ZIO.succeed(
+                                (
+                                  stackTrace,
+                                  toStringOutput,
+                                  printStackTraceOutput
+                                )
+                              )
+                            }
 
-      (stackTrace: String, toStringOutput: String, printStackTraceOutput: String) <- fiberFailureTest
-      _ <- ZIO.succeed {
-             val expectedStackTrace = """zio.FiberSpec.spec.subcall(FiberSpec.scala:line)
-                                         java.base/java.lang.Thread.getStackTrace(Thread.java:line)
-                                         zio.FiberFailure.<init>(FiberFailure.scala:line)
-                                         zio.FiberFailure$.apply(FiberFailure.scala:line)
-                                         zio.Exit.getOrThrowFiberFailure(ZIO.scala:line)
-                                         zio.FiberSpec$.$anonfun$spec$134(FiberSpec.scala:line)
-                                         zio.FiberRuntime.runLoop(FiberRuntime.scala:line)"""
+      tuple <- fiberFailureTest.map {
+                 case (stackTrace: String, toStringOutput: String, printStackTraceOutput: String) =>
+                   (stackTrace, toStringOutput, printStackTraceOutput)
+               }
+      (stackTrace, toStringOutput, printStackTraceOutput) = tuple
+      result <- ZIO.succeed {
+                  val expectedStackTrace = """zio.FiberSpec.spec.subcall(FiberSpec.scala:line)
+                                              java.base/java.lang.Thread.getStackTrace(Thread.java:line)
+                                              zio.FiberFailure.<init>(FiberFailure.scala:line)
+                                              zio.FiberFailure$.apply(FiberFailure.scala:line)
+                                              zio.Exit.getOrThrowFiberFailure(ZIO.scala:line)
+                                              zio.FiberSpec$.$anonfun$spec$134(FiberSpec.scala:line)
+                                              zio.FiberRuntime.runLoop(FiberRuntime.scala:line)"""
 
-             assertTrue(
-               stackTrace.contains("zio.FiberSpec.spec.subcall"),
-               stackTrace.contains("zio.FiberFailure.<init>"),
-               stackTrace.contains("zio.FiberRuntime.runLoop"),
-               stackTrace == expectedStackTrace.trim,
-               toStringOutput.contains(stackTrace),
-               printStackTraceOutput.contains(stackTrace)
-             )
-           }
-    } yield ()
+                  assertTrue(
+                    stackTrace.contains("zio.FiberSpec.spec.subcall"),
+                    stackTrace.contains("zio.FiberFailure.<init>"),
+                    stackTrace.contains("zio.FiberRuntime.runLoop"),
+                    stackTrace == expectedStackTrace.trim,
+                    toStringOutput.contains(stackTrace),
+                    printStackTraceOutput.contains(stackTrace)
+                  )
+                }
+    } yield result
 }
