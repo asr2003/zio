d3fcf659e7d3abbfe8031d00b77bcf25e7bb1052
asr2003 <162500856+asr2003@users.noreply.github.com>
2024-08-30 17:19:03 +0530

    Update FiberFailure.scala

--- core/shared/src/main/scala/zio/FiberFailure.scala
+++ core/shared/src/main/scala/zio/FiberFailure.scala
@@ -58,7 +58,7 @@ final case class FiberFailure(cause: Cause[Any]) extends Throwable(null, null, t
 
   override def toString: String = {
     val stackTraceString = getStackTrace().mkString("\n\tat ", "\n\tat ", "")
-    s"${cause.prettyPrint}\nStack trace:$stackTraceString"
+    s"Exception in thread \"${Thread.currentThread.getName}\" ${cause.prettyPrint}\nStack trace:$stackTraceString"
   }
 
   override def printStackTrace(s: PrintStream): Unit =
